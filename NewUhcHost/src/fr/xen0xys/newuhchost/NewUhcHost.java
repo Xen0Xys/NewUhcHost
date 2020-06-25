c5afade7a661cf9c2f647869d49a6bbb0feef2d0
Xen0Xys <red57101@gmail.com>
2020-06-25 11:13:42 +0200

    Register events

--- NewUhcHost/src/fr/xen0xys/newuhchost/NewUhcHost.java
+++ NewUhcHost/src/fr/xen0xys/newuhchost/NewUhcHost.java
@@ -1,9 +1,11 @@
 package fr.xen0xys.newuhchost;
 
+import fr.xen0xys.newuhchost.events.OnPlayerJoin;
+import fr.xen0xys.newuhchost.events.OnPlayerQuit;
 import fr.xen0xys.newuhchost.models.User;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -24,7 +26,14 @@ public class NewUhcHost extends JavaPlugin {
         super.onEnable();
         instance = this;
         this.saveDefaultConfig();
+        registerEvents();
         addUsersToUserList();
+    }
+
+    private void registerEvents(){
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new OnPlayerJoin(), this);
+        pm.registerEvents(new OnPlayerQuit(), this);
     }
 
     public static NewUhcHost getInstance(){

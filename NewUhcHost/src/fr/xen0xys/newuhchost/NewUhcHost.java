0ba15450d5862817bc87582bce24c63bdc253910
Xen0Xys <red57101@gmail.com>
2020-06-25 10:49:05 +0200

    Initial working plugin

---
+++ NewUhcHost/src/fr/xen0xys/newuhchost/NewUhcHost.java
@@ -0,0 +1,16 @@
+package fr.xen0xys.newuhchost;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NewUhcHost extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
